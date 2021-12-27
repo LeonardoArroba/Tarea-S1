c981e7995bf0d7754fa8ef7aaa9d080a7532a202
LeonardoArroba <86578215+LeonardoArroba@users.noreply.github.com>
2021-12-26 22:13:48 -0500

    Update Ejercicio7.py

--- 1. Tipos de datos y acciones elementales/Ejercicio7.py
+++ 1. Tipos de datos y acciones elementales/Ejercicio7.py
@@ -2,9 +2,7 @@
 # El bit de paridad es 1 si el número de bits 1 es impar y 0 en caso contrario.
 
 numero = int(input("ingrese un numero binario: "))
-
-# if numero % 2 == 1:
-#     print(numero)
-#     print("El número es par.")
-# else:
-#     print("El número es impar.")
+if numero == 1:
+    print("El número es par: {}".format(numero))
+else:
+    print("El número es impar: {}".format(numero))
